a93695459e48164682d47686d1ac8da4a8d3d9d3
qqfunc <148628110+qqfunc@users.noreply.github.com>
2024-08-16 22:32:55 +0900

    Add a workflow file

---
+++ geturl.py
@@ -0,0 +1,9 @@
+"""Get a download URL for an installed version of Python"""
+
+import platform
+import sys
+
+version1 = ".".join(map(lambda n: str(n), sys.version_info[0:3]))
+version2 = platform.python_version()
+
+print("https://www.python.org/ftp/python/{version1}/Python-{version2}.tgz")
