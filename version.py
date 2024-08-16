17f43f039ae7cdf0cfc57835e43e2dd50d431cc7
qqfunc <148628110+qqfunc@users.noreply.github.com>
2024-08-17 00:46:16 +0900

    Fix version.py

--- version.py
+++ version.py
@@ -7,9 +7,9 @@ import sys
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(__doc__)
     parser.add_argument("mode", choices=["long", "short"])
-    parser.parse_args()
+    args = parser.parse_args()
 
-    if parser.mode == "long":
+    if args.mode == "long":
         print(platform.python_version())
     else:
         ".".join(map(lambda n: str(n), sys.version_info[0:3]))
